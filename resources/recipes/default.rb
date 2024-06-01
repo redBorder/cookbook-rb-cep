0c3967f595be0a71ab427407fdc3db4b3d9a4ea5
Miguel Negron <manegron@redborder.com>
2024-06-01 10:37:43 +0100

    lint recipes

--- resources/recipes/default.rb
+++ resources/recipes/default.rb
@@ -1,12 +1,9 @@
-#
-# Cookbook Name:: rbcep
+# Cookbook:: rbcep
 # Recipe:: default
-#
-# redborder
-#
-#
+# Copyright:: 2024, redborder
+# License:: Affero General Public License, Version 3
 
-rbcep_config "config" do
-  name node["hostname"]
+rbcep_config 'config' do
+  name node['hostname']
   action :add
 end
